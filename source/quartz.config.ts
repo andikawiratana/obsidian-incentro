8672170f8cc1d3edf398d7955cd3207b48dbbc04
andika wiratana <andika.wiratana@incentro.com>
2026-01-19 14:55:56 +0800

    update quartz.config.ts

--- source/quartz.config.ts
+++ source/quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "andikawiratana.github.io/obsidian-incentro/,
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
